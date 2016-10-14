b6f860c173e28a7d7cf19a703980511f6b5fb9fe
Joe Haddad <timer150@gmail.com>
2016-10-14 01:09:06 -0400

    Initialize data-attr if browser event has already occurred

--- src/modules/data-attr.js
+++ src/modules/data-attr.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-	document.addEventListener('DOMContentLoaded', require('../../lib/init')({
+	const routine = require('../../lib/init')({
 		selector: {
 			share: '[data-open-share]:not([data-open-share-node])',
 			count: '[data-open-share-count]:not([data-open-share-node])'
@@ -8,5 +8,19 @@ module.exports = function() {
 			share: require('../../lib/initializeShareNode'),
 			count: require('../../lib/initializeCountNode')
 		}
-	}));
+	});
+
+	function detach() {
+		document.removeEventListener('DOMContentLoaded');
+		window.removeEventListener('load');
+
+		routine();
+	}
+
+	if (document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)) {
+		window.setTimeout(routine);
+	} else {
+		document.addEventListener('DOMContentLoaded', detach);
+		window.addEventListener('load', detach);
+	}
 };
